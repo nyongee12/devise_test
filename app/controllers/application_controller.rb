4b198b0331c2a9e7ccde6238fe0cd69926ccb175
nyongee12 <nyongee12@gmail.com>
2015-08-20 05:02:17 +0000

    finish lecture

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  # protect_from_forgery with: :exception
+
+	before_action :add_column, if: :devise_controller?
+
+  protected
+
+  def add_column
+    registration_params = [:name, :age, :gender, :email, :password, :password_confirmation]
+    @page = 1
+    if params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+        |u| u.permit(registration_params)
+      }
+    elsif params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) {
+        |u| u.permit(registration_params << :current_password)
+      }
+    end
+  end
+
+end
